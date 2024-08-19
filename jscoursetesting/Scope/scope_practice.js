bf90bee048bc014e571a8ff212890115008a62e7
shivam123-dev <shivam02bhu@gmail.com>
2024-08-19 11:13:37 -0400

    Scope of variables practice excercise

---
+++ jscoursetesting/Scope/scope_practice.js
@@ -0,0 +1,15 @@
+{
+    //Declaring variables
+    let variable1 = "Shivam";
+    const variable2 = "is a";
+    var variable3 = "good boy";
+
+    //Reassigning these variables
+    variable1 = "Shivaay";
+    variable2 = "hello";  // Throws TypeError: Assignment to constant variable.
+    variable3 = "naughty boy";
+}
+
+variable1 = "Hello";
+variable2 = "Purana";
+variable3 = "hai";
