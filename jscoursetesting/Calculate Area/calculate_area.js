149301db28329b3fe7ed15c8eea5303e0c7d736e
shivam123-dev <shivam02bhu@gmail.com>
2024-08-24 04:24:19 -0400

    Some changes

--- jscoursetesting/Calculate Area/calculate_area.js
+++ jscoursetesting/Calculate Area/calculate_area.js
@@ -2,7 +2,7 @@ let length, width;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(DOCUMENT.getElementById('width').value);
+    width = parseFloat(document.getElementById('width').value);
     let area = length*width;
-    document.getElementById('result').innerText = `Area:  + ${area}`;
+    document.getElementById('result').innerText = `Area:  ${area}`;
 }
